a2ca2eca4cc603824cc35cd97a454fc33cde53e7
Keo <kthorng@gmail.com>
2018-11-12 13:59:41 -0800

    add id to buttons, more style

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -14,7 +14,7 @@ $(function() {
         data: newDevouredState
       }).then(
         function() {
-          console.log("changed sleep to", newDevoured);
+          console.log("changed devoured to", newDevoured);
           // Reload the page to get the updated list
           location.reload();
         }
